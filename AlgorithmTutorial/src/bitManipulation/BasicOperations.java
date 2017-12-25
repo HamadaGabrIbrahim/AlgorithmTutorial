5eb10a115b0269143a26905acc0f78c332922d69
Hamada Ibrahim <Hamada.G.Ibrahim@gmail.com>
2017-12-25 10:59:42 -0500

    Implement and test clearAllBitsFromEndToIndexInclusive() method that clear all the bits from the end until an index inclusive

--- AlgorithmTutorial/src/bitManipulation/BasicOperations.java
+++ AlgorithmTutorial/src/bitManipulation/BasicOperations.java
@@ -23,6 +23,8 @@ public class BasicOperations {
         System.out.println("Put 1 bit in the index 5 of the number 7 and return the result " + setBit(7, 5));
    
         System.out.println("Clear the bit index 2 from the number 14 (14 is 0 0 0 0 1 1 1 0) and after the clear it will be (0 0 0 0 1 0 1 0) the result should be 10, the result is : " + clearBit(14, 2));
+        
+        System.out.println("Clear all the bits from the end until index i inclusive, 15 is (0 0 0 0 1 1 1 1) so clearing until the index 2 will be (0 0 0 0 0 0 1 1) which is 3 , the result is " + clearAllBitsFromEndToIndexInclusive(15, 2));
     }
  
    public static boolean getBitAtdigitNumber(int num, int digitNumber) {
@@ -36,5 +38,9 @@ public class BasicOperations {
    
    public static int clearBit(int num, int index) {
        return (~(1 << index) & num);
+   }
+   
+   public static int clearAllBitsFromEndToIndexInclusive(int num, int index) {
+       return num & ((1 << index) - 1);
    }
 }
