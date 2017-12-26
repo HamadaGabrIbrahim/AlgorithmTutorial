535268fa5b09eb1e3520d404139f8285987a5c5e
MACROSOFTINC\hibrahim <Hamada.G.Ibrahim@gmail.com>
2017-12-25 19:41:55 -0500

    Implement and test a method to find how many ways the child can run in the stairs with dynamic programming

--- AlgorithmTutorial/src/dynamicProgramming/ChildStairs.java
+++ AlgorithmTutorial/src/dynamicProgramming/ChildStairs.java
@@ -12,29 +12,47 @@
 
 package dynamicProgramming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author hibrahim
  */
 public class ChildStairs {
     public static void main(String[] args) {
-       System.out.println("The number of ways for 3 steps is : " + getNumberOfWaysRecursion(3));
+       System.out.println("The number of ways for 3 steps recursion is : " + getNumberOfWaysRecursion(3));
+       System.out.println("The number of ways for 3 steps dynamic programming is : " + getNumberOfWaysDP(3));
        
-       System.out.println("The number of ways for 4 steps is : " + getNumberOfWaysRecursion(4));
+       System.out.println("The number of ways for 4 steps recursion is : " + getNumberOfWaysRecursion(4));
+       System.out.println("The number of ways for 4 steps dynamic programming is : " + getNumberOfWaysDP(4));
        
-       System.out.println("The number of ways for 5 steps is : " + getNumberOfWaysRecursion(5));
+       System.out.println("The number of ways for 5 steps recursion is : " + getNumberOfWaysRecursion(5));
+       System.out.println("The number of ways for 5 steps dynamic programming is : " + getNumberOfWaysDP(5));
     }
     
     public static int getNumberOfWaysRecursion(int num) {
-        
-        if(num < 0)
+        if (num < 0)
             return 0;
-        
-        if(num == 0)
+        if (num == 0)
             return 1;
-        
-        return getNumberOfWaysRecursion(num - 1) + getNumberOfWaysRecursion(num - 2)+ getNumberOfWaysRecursion(num - 3);
-        
+        return getNumberOfWaysRecursion(num - 1) + getNumberOfWaysRecursion(num - 2) + getNumberOfWaysRecursion(num - 3);
         
     } 
+    
+    public static int getNumberOfWaysDP(int num) {
+        int[] previousResultsMap = new int[num + 1];
+        return getNumberOfWaysDP(num, previousResultsMap);
+    }
+    
+    public static int getNumberOfWaysDP(int num, int[] previousResultsMap) {
+        if (num < 0)
+            return 0;
+        if (num == 0)
+            return 1;
+        if (previousResultsMap[num] > 0)
+            return previousResultsMap[num];
+        previousResultsMap[num] = getNumberOfWaysDP(num - 1, previousResultsMap) + getNumberOfWaysDP(num - 2, previousResultsMap) + getNumberOfWaysDP(num - 3, previousResultsMap);
+        return previousResultsMap[num];
+    }
 }
