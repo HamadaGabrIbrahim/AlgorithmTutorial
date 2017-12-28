bbf564495734ee5426a5964d949e7a7cf2a41c1b
Hamada Ibrahim <Hamada.G.Ibrahim@gmail.com>
2017-12-27 20:03:41 -0500

    Implement and test getAllPermutation of a string method

---
+++ AlgorithmTutorial/src/dynamicProgramming/AllPermutationOfString.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dynamicProgramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author hibrahim
+ */
+public class AllPermutationOfString {
+    public static void main(String[] args) {
+        
+        getAllPermutationsOfString("").forEach(word->{
+            System.out.println(word);
+        });
+      
+        
+        System.out.println();
+       
+        getAllPermutationsOfString("a").forEach(word->{
+            System.out.println(word);
+        });
+      
+        
+        System.out.println();
+       
+        getAllPermutationsOfString("ab").forEach(word->{
+            System.out.println(word);
+        });
+      
+        
+        System.out.println();
+       
+         getAllPermutationsOfString("abd").forEach(word->{
+            System.out.println(word);
+        });
+        
+        System.out.println();
+       
+        getAllPermutationsOfString("hamada").forEach(word->{
+            System.out.println(word);
+        });
+        
+    }
+    
+    public static List<String> getAllPermutationsOfString(String str) {
+        return getAllPermutationsOfString(str, 0);
+    }
+    
+    
+    public static List<String> getAllPermutationsOfString(String str, int index) {
+        List<String> allPermutations = new ArrayList<>();
+        
+        if(index == str.length())
+        {
+            allPermutations.add("");
+        }else{
+            
+            char first = str.charAt(index);
+            
+            List<String> words = getAllPermutationsOfString(str, index + 1);
+            
+            words.forEach(word->{
+                for (int j = 0; j <= word.length(); j++) {
+                    String newWord = insertCharAt(word, first, j);
+                    allPermutations.add(newWord);
+                }
+            });
+            
+        }
+        
+        
+        return allPermutations;
+    }
+    
+    public static String insertCharAt(String word, char character, int index) {
+        
+        String before = word.substring(0, index);
+        String after = word.substring(index);
+        return before + character + after;
+        
+        
+    }
+}
+
